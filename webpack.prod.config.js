cd3be6bb675836ec385168eb6138beb0a8843cb8
davides <desiobu@gmail.com>
2017-03-29 20:06:27 -0700

    Clean up package.json and reduce bundle size

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,6 +5,11 @@ const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const config = require('./webpack.config');
 
 config.plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
   new UglifyJsPlugin(),
   new CopyWebpackPlugin([
     { from: './src/index.html', to: 'index.html' },
